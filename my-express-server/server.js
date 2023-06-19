70040152d7377512035182ed84eb08f7927ab23e
gitjdrew <judeandrewd.reyes@gmail.com>
2023-06-19 21:44:23 +0800

    done doing some get request using node.js and express.js. started doing calculator project

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/')
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World!</h1>");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Contact me at example@gmail.com");
+})
+
+let bio = "My name is Jude Andrew Reyes. A Full-Stack Web Developer that has knowledge in multiple languages from front-end, back-end, and database design.";
+
+app.get("/about", (req, res) => {
+    res.send(bio);
+})
+
+app.get("/hobbies", (req, res) => {
+    res.send("Planet hunting, Galaxy hopping, Universal fishing");
+})
 
 app.listen(port, () => {
     console.log("server started on port " + port);
