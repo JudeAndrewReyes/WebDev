bbc1e32127e5a554cfa7b07ac0f5b7cb438af7d0
JudeAndrewReyes <judeandrewd.reyes@gmail.com>
2023-06-21 15:26:48 +0800

    on the process of doing newletter

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+    
+    let firstName = req.body.fname;
+    let lastName = req.body.lname;
+    let email = req.body.email;
+
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
 
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=Batangas&appid=0a3242b7a18b0ce48d4092951512825b&units=metric";
+    res.sendFile(__dirname + '/index.html');
+    
+});
+
+app.post("/", (req, res) => {
+
+    const query = req.body.cityName;
+    const apiKey = "0a3242b7a18b0ce48d4092951512825b";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=" + unit;
     https.get(url, (response) => {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imgUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            res.setHeader('Content-Type', 'text/html');
+            res.write("<h3>The weather is currently " + description + ".</h3>");
+            res.write("<h1>The temperature in "+query+" is " + temp + " degrees Celsius.</h1>");
+            res.write("<img src="+imgUrl+">");
+            res.send();
+        })
     });
 
-    res.send("Welcome");
-});
+})
+
+    
 
 
 app.listen(port, () => {
