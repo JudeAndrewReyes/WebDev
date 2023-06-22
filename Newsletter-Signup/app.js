4ff06538f7c12a60a7cd6afb1488a7cbc5019798
JudeAndrewReyes <judeandrewd.reyes@gmail.com>
2023-06-22 15:19:34 +0800

    started doing the newsletter for the second time

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -14,12 +15,60 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     
-    let firstName = req.body.fname;
-    let lastName = req.body.lname;
-    let email = req.body.email;
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/0e094c1971";
+    
+    const options = {
+        method: "POST",
+        auth: "andrew1:d63bc083a18ad3f70e5a0cee1156c188-us10"
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 
 });
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+})
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 });
+
+// API KEY
+// d63bc083a18ad3f70e5a0cee1156c188-us10
+
+// LIST ID
+// 0e094c1971
