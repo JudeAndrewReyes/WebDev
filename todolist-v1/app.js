5fe87d4d4f8ebad8e038c4fca77247cc801a1fe1
JudeAndrewReyes <judeandrewd.reyes@gmail.com>
2023-07-06 16:48:41 +0800

    added header and footer

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,7 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+let items = ["Buy food", "Cook food", "Eat food"];
+let workItems =[];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// use to load the public folder
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 
@@ -19,12 +26,41 @@ app.get("/", (req, res) => {
     let day = today.toLocaleDateString("en-PH", options)
 
     res.render("list", {
-        kindOfDay: day
+        listTitle: day,
+        newListItems: items
     });
 
 });
 
+app.post("/", (req, res) => {
 
+    let item = req.body.newItem;
+    console.log(req.body);
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+app.post("/work", (req, res) => {
+    let item = req.body.newItem;
+
+    workItems.push(item);
+
+    res.redirect("/work");
+});
 
 app.listen(port, () => {
     console.log("listening on port", port);
