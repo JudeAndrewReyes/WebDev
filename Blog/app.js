55454856bb33760fe894bf93b40e9c237f0c6504
JudeAndrewReyes <judeandrewd.reyes@gmail.com>
2023-07-09 14:51:24 +0800

    done blog and started learning db

--- Blog/app.js
+++ Blog/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -8,39 +9,66 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+    const posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent
-  });
+    res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: posts
+    });
 })
 
 app.get("/about", (req, res) => {
-  res.render("about", {
-    aboutContent: aboutContent
-  });
+    res.render("about", {
+        aboutContent: aboutContent
+    });
 })
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {
-    contactContent: contactContent
-  });
+    res.render("contact", {
+        contactContent: contactContent
+    });
 })
 
 app.get("/compose", (req, res) => {
-  res.render("compose");
+    res.render("compose");
 })
 
 app.post("/compose", (req, res) => {
-  const textInput = req.body.postTitle;
 
-  console.log(textInput);
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    }
+
+    posts.push(post);
+    res.redirect("/")
+
+})
+
+app.get("/posts/:postName", (req, res) => {
+
+    const reqTitle = _.lowerCase(req.params.postName);
+    posts.some(post => {
+
+        const lowerCase = _.lowerCase(post.title);
+
+        if (lowerCase === reqTitle) {
+            res.render("post", {
+                title: post.title,
+                content: post.content
+            })
+        }
+
+    });
+
 })
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
